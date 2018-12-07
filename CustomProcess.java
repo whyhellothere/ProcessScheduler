605c5e1370a7b2dbe8797b1db51e8cde7dbcd518
Daniel <dchu22@wisc.edu>
2018-12-06 19:54:21 -0600

    same

--- CustomProcess.java
+++ CustomProcess.java
@@ -1,4 +1,8 @@
 
+/**
+ * Class for Process objects that can be run in Process Scheduler
+ *
+ */
 public class CustomProcess implements Comparable<CustomProcess>{
 
   private static int nextProcessId = 1; // stores the id to be assigned to the next process 
@@ -6,37 +10,51 @@ public class CustomProcess implements Comparable<CustomProcess>{
   private final int PROCESS_ID; // unique identifier for this process
   private int burstTime; // time required by this process for CPU execution
   
+  /**
+   * Constructor for CustomProcess. Initializes all variables and updates the next ProcessId
+   * @param burstTime is the time this process takes to run
+   */
   public CustomProcess(int burstTime) { 
     this.burstTime = burstTime;
     this.PROCESS_ID = nextProcessId;
     nextProcessId++;
   }
   
+  /**
+   * Getter method for ProcessId
+   * @return the Process Id
+   */
   public int getProcessId() {
     return PROCESS_ID;
   }
   
+  /**
+   * Getter method for how long process takes to run
+   * @return how long the process takes to run
+   */
   public int getBurstTime() {
     return burstTime;
   }
 
-  /*
-  p1.compareTo(p2) < 0 means that the p1 has higher priority than p2. So, p1 should be run first.
-  p1.compareTo(p2) == 0 means that p1 and p2 have exactly the same priority.
-  p1.compareTo(p2) > 0 means that p1 has lower priority than p2.
-  */
+  /**
+   * Compares one CustomProcess to another CustomProcess. Returns 1 if the other has higher
+   * priority. Returns -1 if the other has lower priority. Returns -1 if burst times for each are
+   * the same and other has lower process id. Returns 1 if burst times are the same and other has
+   * higher process id
+   * @param other is the other CustomProcess being compared to
+   */
   public int compareTo(CustomProcess other) {
-    int compare = (burstTime - other.burstTime);
-    if (compare == 0) {
+    int compare = (burstTime - other.burstTime); // finds difference in times
+    if (compare == 0) { // if times are equal
       if (PROCESS_ID < other.PROCESS_ID) {
-        return -1;
+        return -1; // return -1 if this has smaller process id
       }
-      return 1;
+      return 1; // return 1 if this has bigger process id
     }
-    if (compare < 0) {
-      return -1;
+    if (compare < 0) { // if difference is negative
+      return -1; // return -1
     }
-    return 1;
+    return 1; // else if difference is positive, return 1
   }
   
   
